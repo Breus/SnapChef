8ca8050f2458b06f3c4d6dc45714ea2aa0bf1b91
breus <b.blaauwendraad@gmail.com>
2025-07-15 22:17:39 +0200

    Use dev profile for local development in Quarkus container

--- app/src/main/java/dev/blaauwendraad/recipe_book/resource/RecipeResource.java
+++ app/src/main/java/dev/blaauwendraad/recipe_book/resource/RecipeResource.java
@@ -1,10 +1,13 @@
 package dev.blaauwendraad.recipe_book.resource;
 
 import dev.blaauwendraad.recipe_book.dto.RecipeDTO;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import java.util.List;
 
@@ -13,17 +16,22 @@ public class RecipeResource {
 	private final List<RecipeDTO> recipes = RecipeMockData.getMockRecipes();
 
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<RecipeDTO> list() {
 		return recipes;
 	}
 
 	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
 	public List<RecipeDTO> add(RecipeDTO recipe) {
 		recipes.add(recipe);
 		return recipes;
 	}
 
 	@DELETE
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
 	public List<RecipeDTO> remove(RecipeDTO recipe) {
 		recipes.removeIf(existingRecipe -> existingRecipe.id().equals(recipe.id()));
 		return recipes;
