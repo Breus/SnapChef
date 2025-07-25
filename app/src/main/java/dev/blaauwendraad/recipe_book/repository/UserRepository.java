cfd15c2fffcf857ae295bff230301f3ad79f37e7
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2025-07-25 18:01:28 +0200

    Refactor UserRepository and UserService to use UserAccount model

--- app/src/main/java/dev/blaauwendraad/recipe_book/repository/UserRepository.java
+++ app/src/main/java/dev/blaauwendraad/recipe_book/repository/UserRepository.java
@@ -2,29 +2,63 @@ package dev.blaauwendraad.recipe_book.repository;
 
 import dev.blaauwendraad.recipe_book.data.model.RoleEntity;
 import dev.blaauwendraad.recipe_book.data.model.UserAccountEntity;
+import dev.blaauwendraad.recipe_book.service.model.UserAccount;
+import dev.blaauwendraad.recipe_book.service.model.UserRole;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class UserRepository {
     @Transactional
-    public UserAccountEntity createUser(
-            String username, String passwordHash, String emailAddress, Set<RoleEntity> roles) {
+    public UserAccount createUser(String username, String passwordHash, String emailAddress, Set<RoleEntity> roles) {
         UserAccountEntity user = new UserAccountEntity();
         user.username = username;
         user.passwordHash = passwordHash;
         user.emailAddress = emailAddress;
         user.roles = roles;
         user.persist();
-        return user;
+
+        return toUserAccount(user);
     }
 
-    public UserAccountEntity findByUsername(String username) {
-        return UserAccountEntity.find("username", username).firstResult();
+    public UserAccount findById(Long id) {
+        UserAccountEntity user = UserAccountEntity.findById(id);
+        if (user == null) {
+            return null;
+        }
+        return toUserAccount(user);
     }
 
-    public UserAccountEntity findByEmail(String emailAddress) {
-        return UserAccountEntity.find("emailAddress", emailAddress).firstResult();
+    public UserAccount findByUsername(String username) {
+        UserAccountEntity user = UserAccountEntity.find("username", username).firstResult();
+        if (user == null) {
+            return null;
+        }
+        return toUserAccount(user);
+    }
+
+    public UserAccount findByEmail(String emailAddress) {
+        UserAccountEntity user =
+                UserAccountEntity.find("emailAddress", emailAddress).firstResult();
+        if (user == null) {
+            return null;
+        }
+        return toUserAccount(user);
+    }
+
+    private static UserAccount toUserAccount(UserAccountEntity userAccountEntity) {
+        return new UserAccount(
+                userAccountEntity.id,
+                userAccountEntity.username,
+                userAccountEntity.emailAddress,
+                userAccountEntity.passwordHash,
+                userAccountEntity.roles.stream()
+                        .map(role -> switch (role.roleName) {
+                            case admin -> UserRole.administrator;
+                            case user -> UserRole.user;
+                        })
+                        .collect(Collectors.toSet()));
     }
 }
--- app/src/main/java/dev/blaauwendraad/recipe_book/service/UserService.java
+++ app/src/main/java/dev/blaauwendraad/recipe_book/service/UserService.java
@@ -2,7 +2,6 @@ package dev.blaauwendraad.recipe_book.service;
 
 import dev.blaauwendraad.recipe_book.data.model.RoleEntity;
 import dev.blaauwendraad.recipe_book.data.model.RoleName;
-import dev.blaauwendraad.recipe_book.data.model.UserAccountEntity;
 import dev.blaauwendraad.recipe_book.repository.UserRepository;
 import dev.blaauwendraad.recipe_book.service.exception.UserRegistrationException;
 import dev.blaauwendraad.recipe_book.service.exception.UserRegistrationValidationException;
@@ -12,7 +11,6 @@ import io.quarkus.elytron.security.common.BcryptUtil;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class UserService {
@@ -41,14 +39,7 @@ public class UserService {
             throw new UserRegistrationValidationException("Username is already in use.");
         }
         String hashPassword = BcryptUtil.bcryptHash(password);
-        UserAccountEntity userAccountEntity =
-                userRepository.createUser(username, hashPassword, emailAddress, Set.of(RoleEntity.getUserRole()));
-        return new UserAccount(
-                userAccountEntity.id,
-                userAccountEntity.username,
-                userAccountEntity.emailAddress,
-                userAccountEntity.passwordHash,
-                userAccountEntity.roles.stream().map(UserService::toUserRole).collect(Collectors.toSet()));
+        return userRepository.createUser(username, hashPassword, emailAddress, Set.of(RoleEntity.getUserRole()));
     }
 
     private static UserRole toUserRole(RoleEntity roleEntity) {
