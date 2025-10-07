eaff58aa808556331c52721357fd1a285df953cc
Emiel Bos <emielbos@protonmail.com>
2025-10-07 22:08:39 +0200

    Display "Anonymous" when author is NULL (#6)

--- app/db/001-demo-data.sql
+++ app/db/001-demo-data.sql
@@ -32,8 +32,8 @@ INSERT INTO recipe (title, description, num_servings, preparation_time, author_i
   ('Fish Tacos', 'Mexican-style fish tacos with fresh salsa.', 4, 'MIN_30_45', 3),
   ('Greek Salad', 'Traditional Mediterranean salad with feta cheese.', 4, 'HOUR_PLUS', 4),
   ('Beef Stir Fry', 'Quick and easy Asian-style beef dish.', 4, 'HOUR_PLUS', 1),
-  ('Banana Bread', 'Moist and delicious homemade bread.', 4, 'MIN_15_30', 2),
-  ('Vegetable Soup', 'Healthy and warming soup with mixed vegetables.', 4, 'MIN_15_30', 3)
+  ('Banana Bread', 'Moist and delicious homemade bread.', 4, 'MIN_15_30', NULL),
+  ('Vegetable Soup', 'Healthy and warming soup with mixed vegetables.', 4, 'MIN_15_30', NULL)
 ON CONFLICT DO NOTHING;
 
 INSERT INTO ingredient (name, quantity, position, recipe_id) VALUES
--- ui/src/components/RecipeSummary.vue
+++ ui/src/components/RecipeSummary.vue
@@ -25,7 +25,8 @@ const { userId } = useAuth();
                             <path fill-rule="evenodd" d="M11.013 2.513a1.75 1.75 0 0 1 2.475 2.474L6.226 12.25a2.751 2.751 0 0 1-.892.596l-2.047.848a.75.75 0 0 1-.98-.98l.848-2.047a2.75 2.75 0 0 1 .596-.892l7.262-7.261Z" clip-rule="evenodd" />
                         </svg>
                     </span>
-                    <span class="mr-1" v-if="recipeSummary.author.userId == userId"> {{ recipeSummary.author.username }} (Me)</span>
+                    <span class="mr-1" v-if="!recipeSummary.author">Anonymous</span>
+                    <span class="mr-1" v-else-if="recipeSummary.author.userId == userId"> {{ recipeSummary.author.username }} (Me)</span>
                     <span class="mr-1" v-else>{{ recipeSummary.author.username }}</span>
                 </div>
                 <div class="flex items-center">
