f57568712c2761756e662d8ec063b686054608dc
Emiel <emielbos@protonmail.com>
2025-10-07 22:06:09 +0200

    Display "Anonymous" when author is NULL

--- ui/src/components/RecipeView.vue
+++ ui/src/components/RecipeView.vue
@@ -246,7 +246,8 @@ onMounted(async () => {
                                     <path fill-rule="evenodd" d="M11.013 2.513a1.75 1.75 0 0 1 2.475 2.474L6.226 12.25a2.751 2.751 0 0 1-.892.596l-2.047.848a.75.75 0 0 1-.98-.98l.848-2.047a2.75 2.75 0 0 1 .596-.892l7.262-7.261Z" clip-rule="evenodd" />
                                 </svg>
                             </span>
-                            <span class="mr-1" v-if="recipe.author.userId == userId"> {{ recipe.author.username }} (Me)</span>
+                            <span class="mr-1" v-if="!recipe.author">Anonymous</span>
+                            <span class="mr-1" v-else-if="recipe.author.userId == userId"> {{ recipe.author.username }} (Me)</span>
                             <span class="mr-1" v-else>{{ recipe.author.username }}</span>
                         </div>
                         <div class="flex items-center">
