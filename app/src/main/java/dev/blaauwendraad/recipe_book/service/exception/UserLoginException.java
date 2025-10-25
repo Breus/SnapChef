5c45ee12a14db19d728eebd05c93f17bd2b09bda
Emiel <emielbos@protonmail.com>
2025-10-25 13:08:39 +0200

    Added e-mail and password updating

--- app/src/main/java/dev/blaauwendraad/recipe_book/service/exception/UserLoginException.java
+++
@@ -1,8 +0,0 @@
-package dev.blaauwendraad.recipe_book.service.exception;
-
-public class UserLoginException extends DetailedMessageException {
-
-    public UserLoginException(String detailMessage) {
-        super("Failed to login user.", detailMessage);
-    }
-}
